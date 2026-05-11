c44a040e28063387909d05c6bfe1c93329b5a954
eduardolalo9 <eduardolalo9606@gmail.com>
2026-05-11 17:14:34 -0600

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCjFziFbjsxZQq-CzkCsMuYXgZiFVYEXa0",
+  authDomain: "gestor-de-inventarios-76c19.firebaseapp.com",
+  projectId: "gestor-de-inventarios-76c19",
+  storageBucket: "gestor-de-inventarios-76c19.firebasestorage.app",
+  messagingSenderId: "520575498448",
+  appId: "1:520575498448:web:3a83c2e4fcaedfc9d386d5"
+};
+
+firebase.initializeApp(firebaseConfig);
