ecbbc3e0b84d315148d8a91b5a0dc246e6cfda84
eduardolalo9 <eduardolalo9606@gmail.com>
2026-05-11 12:34:44 -0600

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./sw.js')
-    .then(reg => console.log('SW registrado', reg))
-    .catch(err => console.error('Error SW', err));
-}
